7f290e4864b22bf9964fff61ef314c64437ceaf8
sercel123 <57289819+sercel23@users.noreply.github.com>
2019-11-03 00:34:46 +0300

    Update Satir_ici_islemler.py

--- Satir_ici_islemler.py
+++ Satir_ici_islemler.py
@@ -3,6 +3,6 @@ str="Hayat Kısa Python Ögrenin"
 x=[i for i in str if i.isupper()]#str içindeki harflerden büyük harf olanlar
 """
 i.isupper() metodu i'nin büyük harf olup olmadığını kontrol eder 
-ve bollean değer döndürür 
+ve boolean değer döndürür 
 """
 print(x)
