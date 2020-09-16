8a9888b04accd0bdb55b9b19640f5419b3624d9b
Serap ERÇEL <ercelserap@gmail.com>
2020-09-17 00:00:54 +0300

    Add files via upload

---
+++ ExchangeAPI_dovizUyg.py
@@ -0,0 +1,13 @@
+import requests
+import json
+
+api_url="https://api.exchangeratesapi.io/latest?base="
+bozulan_doviz=input("Bozulan döviz türü: ")
+alinan_doviz=input("Alınan döviz türü: ")
+miktar=int(input(f"Ne kadar {bozulan_doviz} bozdurmak istiyorsunuz: "))
+
+result=requests.get(api_url+bozulan_doviz)
+result=json.loads(result.text)
+
+print("1 {0} ={1} {2}".format(bozulan_doviz,result["rates"][alinan_doviz],alinan_doviz))
+print("{0} {1}= {2} {3}".format(miktar,bozulan_doviz,miktar*result["rates"][alinan_doviz],alinan_doviz))
